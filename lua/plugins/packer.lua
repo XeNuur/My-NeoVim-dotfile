e6c30b46bcdf6112f4a190ea6fd8295c124b446f
XeNuul <janlomozik4@gmail.com>
2023-01-25 15:37:54 +0100

    added hop.nvim

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -21,5 +21,15 @@ return require('packer').startup(function()
        'nvim-telescope/telescope.nvim', tag = '0.1.0',
        requires = { {'nvim-lua/plenary.nvim'} }
    }
+
+   use {
+     'phaazon/hop.nvim',
+     branch = 'v2', -- optional but strongly recommended
+     config = function()
+       -- you can configure Hop the way you like here; see :h hop-config
+       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+     end
+   }
+
 end)
 
