774515b9e7ca8a1f32e4158e0552cb8d0b351a63
Jan Łomozik <janlomozik4@gmail.com>
2024-01-17 19:51:02 +0100

    Fixing enter ACROSS the terminal emulators

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -18,7 +18,9 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
+
     ['<C-CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
   },
 
   sources = {
