2f96f8696f37f13bcf3e606f14003c6438b300b8
Jan Łomozik <janlomozik4@gmail.com>
2024-01-17 19:41:53 +0100

    Fixing Control+Enter issue

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -18,7 +18,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+    ['<C-CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
   },
 
   sources = {
